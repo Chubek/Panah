6d3b0ebe2688d69d4c418f4cc0263128f19f277b
Chubak <chubakbidpaa@gmail.com>
2023-05-11 11:03:59 +0430

    fixed some mistakes, added more arg parsing features

--- scripts/dnsrezulf.py
+++ scripts/dnsrezulf.py
@@ -10,8 +10,8 @@
 # based on RFC 1035: https://datatracker.ietf.org/doc/html/rfc1035
 # also RFC RFC 3596 for AAAA: https://datatracker.ietf.org/doc/html/rfc3596
 
-from socket import AF_INET, SOCK_DGRAM, socket, connect, send, receive
-from dataclass import dataclass
+from socket import AF_INET, SOCK_DGRAM, socket
+from dataclasses import dataclass
 from time import time_ns
 
 # Part A: Globals & Utils
@@ -72,38 +72,38 @@ def uint_to_inet_str(inet: int) -> str:
 
 @dataclass 
 class DNSQueryHeader:
-	xid					# a random 16-bit unsigned ID
-	qr					# question or response? One bit only
-	opcode				# four bits. What type of query is it? We always set it to squery (standard querey)
-	aa					# Authoratic Answer or not  --- one bit
-	tc					# has it be truncated? one bit
-	rd					# only in question, is recursion desired? --- one bit
-	ra					# only in response, is recursion available? --- one bit
-	z					# always set to 0 --- three bits
-	rcode				# response code, 4 bits
-	qdcount   			# 16-bit unsigned, question count
-	ancount   			# 16-bit unsigned, answer count
-	nscount 			# 16-bit unsigned, name authority record count
-	arcount   			# 16-bit unsigned, additional record count
+	xid = 0					# a random 16-bit unsigned ID
+	qr = 0					# question or response? One bit only
+	opcode = 0				# four bits. What type of query is it? We always set it to squery (standard querey)
+	aa = 0					# Authoratic Answer or not  --- one bit
+	tc = 0					# has it be truncated? one bit
+	rd = 0					# only in question, is recursion desired? --- one bit
+	ra = 0					# only in response, is recursion available? --- one bit
+	z = 0					# always set to 0 --- three bits
+	rcode = 0				# response code, 4 bits
+	qdcount = 0   			# 16-bit unsigned, question count
+	ancount = 0   			# 16-bit unsigned, answer count
+	nscount = 0 			# 16-bit unsigned, name authority record count
+	arcount = 0   			# 16-bit unsigned, additional record count
 
 # this is format of a question, there can be several, but we'll just send one
 
 @dataclass
 class DNSQueryQuestion:
-	qname				# variable, name of the desired domain, null-terminated
-	qtype				# type of the question, 16-bit unsigned
-	qclass 				# class of the question, 16-bit unsigned
+	qname  = b""			# variable, name of the desired domain, null-terminated
+	qtype  = 0				# type of the question, 16-bit unsigned
+	qclass = 0 				# class of the question, 16-bit unsigned
 
 # this is format of a response resource record, there can be several, but we'll just send one
 
 @dataclass
 class DNSResourceRecord:
-	name				# variable, human-readable name of the domain
-	rtype  				# unsigned 16-bit integer, type of the resource
-	rclass 				# unsigned 16-bit integer, class of the resource
-	ttl    				# unsigned 16-bit integer, time interval until caching is valid
-	rdlength 			# unsigned 16-bit integer, length of rdata
-	rdata	   			# variable, the data, for A and AAA it's the IPV4 and IPV6
+	name      = b""			# variable, human-readable name of the domain
+	rtype     = 0  			# unsigned 16-bit integer, type of the resource
+	rclass    = 0			# unsigned 16-bit integer, class of the resource
+	ttl    	  = 0    		# unsigned 16-bit integer, time interval until caching is valid
+	rdlength  = 0 			# unsigned 16-bit integer, length of rdata
+	rdata     = b""	   		# variable, the data, for A and AAA it's the IPV4 and IPV6
 
 # Part C: DNS Query Protocol
 
@@ -137,16 +137,16 @@ def new_dns_query_header(rd=RECURDESIRE):
 	return DNSQueryHeader(xid=generate_random_ushort(), rd=rd)
 
 # encode the query header, as specified by the RFC
-# the flags are 16-bits, and as the net byte order goes, big-endian
+# the options are 16-bits, and as the net byte order goes, big-endian
 
 def encode_dns_query_header(header: DNSQueryHeader) -> bytearray:
 	xid = header.xid.to_bytes(2, byteorder="big")
-	flags = ((header.qr & 0b1) | ((header.opcode << 15) & 0b1111) | ((header.aa << 11) & 0b1) | ((header.tc << 10) & 0b1) | (header.rd << 9) | ((header.ra << 8) & 0b1) | ((header.z << 7) & 0b1111) | (header.rcode & 0b11111)).to_bytes(2, byteorder='big')
+	options = ((header.qr & 0b1) | ((header.opcode << 15) & 0b1111) | ((header.aa << 11) & 0b1) | ((header.tc << 10) & 0b1) | (header.rd << 9) | ((header.ra << 8) & 0b1) | ((header.z << 7) & 0b1111) | (header.rcode & 0b11111)).to_bytes(2, byteorder='big')
 	qdcount = header.qdcount.to_bytes(2, byteorder='big')
 	ancount = header.anqount.to_bytes(2, byteorder='big')
 	nscount = header.nscount.to_bytes(2, byteorder='big')
 	arcount = header.arcount.to_bytes(2, byteorder='big')
-	return xid + flags + qdcount + ancount + nscount + arcount
+	return xid + options + qdcount + ancount + nscount + arcount
 
 
 def encode_dns_query_question(question: DNSQueryQuestion):
@@ -163,15 +163,15 @@ def encode_dns_query_packet(header: DNSQueryHeader, question: DNSQueryQuestion) 
 
 def decode_dns_query_header(response: bytes) -> DNSQueryHeader:
 	xid = int.from_bytes(response[:2], byteorder="big", signed=False)
-	flags = int.from_bytes(response[2:4], byteorder="big", signed=False)
-	qr = (flags & 32768) >> 15
-	opcode = (flags & 32720) >> 11
-	aa = (flags & 1024) >> 10
-	tc = (flags & 256) >> 9
-	rd = (flags & 128) >> 8
-	ra = (flags & 64) >> 7
-	z  = (flags & 0b1110000) >> 4
-	rcode = (flags & 15)
+	options = int.from_bytes(response[2:4], byteorder="big", signed=False)
+	qr = (options & 32768) >> 15
+	opcode = (options & 32720) >> 11
+	aa = (options & 1024) >> 10
+	tc = (options & 256) >> 9
+	rd = (options & 128) >> 8
+	ra = (options & 64) >> 7
+	z  = (options & 0b1110000) >> 4
+	rcode = (options & 15)
 	qdcount = int.from_bytes(response[4:6], byteorder="big", signed=False)
 	ancount = int.from_bytes(response[6:8], byteorder="big", signed=False)
 	nscount = int.from_bytes(response[8:10], byteorder="big", signed=False)
@@ -210,21 +210,22 @@ def generate_and_compose_query(address: str, rectype=ARECORD, recursive=RECURDES
 
 def parse_server_response(response: bytearray, xid: int, recursion=RECURDESIRE) -> bytes:
 	header = decode_dns_query_header(response)
+	# check for errors in response
 	if header.xid != xid:
-		return ERRXIDMATCH
+		return ERRXIDMATCH					# the XID given does not match with XID returned
 	if header.ra != recursion:
-		return ERRRECUR
+		return ERRRECUR						# if we have set recursion to true, and server does not support it
 	if header.rcode != NOERR:
-		if header.rcode == SERVERFAIL:
+		if header.rcode == SERVERFAIL:      # server fail
 			return ERRSERVER
-		elif header.rcode == NAMEERR:
-			return ERRNAME
-		elif header.rcode == FORMATERROR:
+		elif header.rcode == NAMEERR:       # name error
+			return ERRNAME				
+		elif header.rcode == FORMATERROR:   # format error
 			return ERRFORMAT
 		else:
-			return -1 * header.rcode
+			return -1 * header.rcode        # we just check for the most common errors. We sign-extend the other non-compliant rcodes and return them
 	record = decode_dns_query_resource_record(response)
-	return record.rdata
+	return record.rdata            			# we return the rdata, other parts are not desired
 
 
 # Part D: Resolver
@@ -235,24 +236,24 @@ class DNSResolver:
 	def __init__(self, resolver="8.8.8.8", port=53):
 		self.resolver = resolver
 		self.port = port
-		self.socket = socket(AF_INET, SOCK_DGRAM)
+		self.socket = socket(AF_INET, SOCK_DGRAM)							# open a socket to the resolver server
 
 	def connect_to_resolver(self):
-		self.socket.connect(self.resolver, self.port)
+		self.socket.connect(self.resolver, self.port)						# connect to the socket
 
 	def send_and_receive_query(self, addr: str, rectype=ARECORD, recursion=RECURDESIRE, retries=3) -> bytes:
-		packet, xid = generate_and_compose_query(addr, rectype, recursion)
+		packet, xid = generate_and_compose_query(addr, rectype, recursion)  # generate the packet
 		lenpaacket = len(packet)
-		sent = self.socket.send(packet)
-		while sent != lenpacket and retries > 0:
+		sent = self.socket.send(packet)										# send the packet
+		while sent != lenpacket and retries > 0:							# retry if send fails
 			sent = self.socket.send(packet)
 		response = []
 		while True:
-			received_data = self.recv(1024)
+			received_data = self.recv(1024)									# get the response
 			if received_data is None:
 				break
 			response.extend(received_data)
-		return parse_server_response(response, xid, recursion)
+		return parse_server_response(response, xid, recursion)				# parse the response
 
 	def close_connection(self):
 		self.socket.shutdown()
@@ -264,35 +265,43 @@ if __name__ == "__main__":
 	from sys import argv, executable
 	execname = executable.split("/")[-1]
 	scriptname = argv[0]
+
+	options = []										# we pre-declare the options list because we wanna reference it to print_help_and_exit
 	
-	flags = [
-		("--address", "-ad", "example.com", str, "The address to resolve"),
-		("--resolver", "-rs", "8.8.8.8", str, "The DNS resolver"),
-		("--port", "-p", 53, int, "The resolver port"),
-		("--record-type", "-rt", "A", choose_record_type, "The query record (A, AAAA or CNAME)"),
-		("--recursion", "-rc", True, choose_recursion, "Recursion flag (no argument)"),
-		("--help", "-h", "N/A", print_help_and_exit, "Show help")
-
-	]
-
-	def choose_record_type(rectype: str) -> int:
+	def choose_record_type(_, rectype: str) -> int:	# this function parses the record type
 		if rectype == "A":
 			return ARECORD, "A"
 		elif rectype == "AAAA":
 			return AAAARECORD, "AAAA"
-		elif rectype = "CNAME":
+		elif rectype == "CNAME":
 			return CNAMERECORD, "CNAME"
 		else:
 			error_out("Wrong record type, can only be A, AAAA, or CNAEM")
 
-	def choose_recursion(rec: bool) -> int:
+	def choose_recursion(rec: bool) -> int:				# this function parses the recursion flag
 		if rec:
 			return RECURDERSIRE
 		else:
 			return RECURNOTDES
+  														# this function error-checks string options
+	def set_string_option(arg: str, option: str) -> str: 
+		if arg is None:									# for both this and the next one, we return
+			return option 								# the option if the first parameter is None
+		if option.isdigit(): 							# so that we can pass defaults to them too
+			error_out(f"Numeric argument passed for {arg}")
+		return option
+														# this function error-checks numeric arguments
+	def set_integer_option(arg: str, option: str, range=(0, 65535)) -> int:
+		if arg is None:
+			return option
+		if not option.isdigit():
+			error_out(f"Non-numeric argument passed for {arg}")
+		parsed = int(option)
+		if parsed < range[0] or parsed > range[1]:
+			error_out(f"Argument passed for {arg} is not in range ([{range[0]}, {range[1]}])")
 
-	def print_help_and_exit(_):
-		argmsg = "\n".join([f"[{l}/{s}]\t{h}\tDefault: {d}" for l, s in [(f[0], f[1], f[4], f[2]) for f in flags]])
+	def print_help_and_exit(_, options=options):		# this function is for composing and printing help
+		argmsg = "\n".join([f"[{l}/{s}]\t{h}\tDefault: {d}" for l, s in [(f[0], f[1], f[4], f[2]) for f in options]])
 		print("\033[1;33mDNS Resolver by Chubak Bidpaa\033[0m")
 		print("Usage:")
 		print(execname, scriptname, argmsg)
@@ -300,32 +309,54 @@ if __name__ == "__main__":
 		print(execname, scriptname, "--record-type AAAA")
 		exit(1)
 
-	def parse_arg_or_default(arg, next_arg, long, short, default, func, _, _):
+
+													# arguments/options in form (long, short, default, parser, help)
+	options = [
+		("--address", "-ad", "example.com", set_string_option, "The address to resolve"),
+		("--resolver", "-rs", "8.8.8.8", set_string_option, "The DNS resolver"),
+		("--port", "-p", 53, set_integer_option, "The resolver port"),
+		("--record-type", "-rt", "A", choose_record_type, "The query record (A, AAAA or CNAME)"),
+		("--recursion", "-rc", True, choose_recursion, "Recursion flag (no argument)"),
+		("--retries", "-re", 5, lambda *a, **k: set_integer_options(*a, **k, range=(0, 30)), "Number of retries in case connection fails"),
+		("--help", "-h", "N/A", print_help_and_exit, "Show help")
+
+	]
+
+													  # this function parses the arguments, based on the following argument
+													  # we must give it one of the flag list members, expanded with asterisk
+	def parse_arg_or_default(arg, next_arg, long, short, default, func, _, __):
 		if arg == long or arg == short:
-			return func(next_arg)
+			return func(arg, next_arg)
+		else:
+			return func(None, default)
+
+													  # same as before, except it does not take the next argument, since it's a flipper
+	def parse_flag_or_default(arg, long, short, defualt, func, _, __):
+		if arg == long or arg == short:
+			return func(True)						  # if the flag has been passed, this means it's true
 		else:
 			return func(default)
 
+	argv.append(None)								  # we must append a dummy to argv, it has be default one member and we need two to set our options
 
-	def parse_flag_or_default(arg, long, short, defualt, func, _, _):
-		if arg == long or arg == short:
-			return func(bool(arg))
-		else:
-			return func(default)
+	for arg, next_arg in zip(argv[::2], argv[1::2]):  # we zip even and odd members of argv
+		address = parse_arg_or_default(arg, next_arg, *options[0])
+		resolver = parse_arg_or_default(arg, next_arg, *options[1])
+		port = parse_arg_or_default(arg, next_arg, options[2])
+		rectype, strrectype = parse_arg_or_default(arg, next_arg, options[3])
+		recursion = parse_flag_or_default(arg, *options[4])
+		retries = parse_arg_or_default(arg, next_arg, *options[5])
+		_ = parse_arg_or_default(arg, *options[6])
+													   # this loop will set the default values either way
 
-	for arg, next_arg in zip(argv[::2], argv[1::2]):
-		address = parse_arg_or_default(arg, next_arg, *flags[0])
-		resolver = parse_arg_or_default(arg, next_arg, *flags[1])
-		port = parse_arg_or_default(arg, next_arg, flags[2])
-		rectype, strrectype = parse_arg_or_default(arg, next_arg, flags[3])
-		recursion = parse_flag_or_default(arg, *flags[4])
-		_ = parse_arg_or_default(arg, *flags[5])
-
+	# final showdown
 
 	dnsresolver = DNSResolver(resolver, port)
 	dnsresolver.connect_to_resover()
-	data = dnsresolver.send_and_receive_query(rectype, recursion)
+	data = dnsresolver.send_and_receive_query(address, rectype, recursion, retries)
 	dnsresolver.close_connection()
+
+	# checking for errors that we set before in case of something going wrong	
 
 	if data < 0:
 		if data == ERRXIDMATCH:
@@ -344,7 +375,7 @@ if __name__ == "__main__":
 
 	if strrectype == "A":
 		print(f"{strrectype}\t\t|{uint_to_inet_str(int(data))}")
-	elif strrectype == "CNAME"
+	elif strrectype == "CNAME":
 		print(f"{strrectype}\t\t|{data}")
 	else:
 		print(f"{strrectype}\t\t|{int(data)}")
