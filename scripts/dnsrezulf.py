3bf5bcac4bb6e0723b47f8623ff3a740f35e0ef2
Chubak <chubakbidpaa@gmail.com>
2023-05-13 08:25:30 +0430

    hex for ipv6

--- scripts/dnsrezulf.py
+++ scripts/dnsrezulf.py
@@ -350,6 +350,6 @@ if __name__ == "__main__":
 	if rectype == RECORD_A:
 		print(f"\t{address} | A | {ttl} | {'.'.join([str(c) for c in data])}")
 	elif rectype == RECORD_AAAA:
-		print(f"\t{address} | AAAA | {ttl} | {':'.join([str(c) for c in data])}")
+		print(f"\t{address} | AAAA | {ttl} | {':'.join([format(c, 'x') for c in data])}")
 	else:
 		print(f"\t{address} | CNAME | {ttl} | {data}")
