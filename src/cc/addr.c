389727ed22c61a185bc8b0f3af7c7c4a17567efd
Chubak <chubakbidpaa@gmail.com>
2023-05-13 14:35:39 +0430

    changes to address

--- src/cc/addr.c
+++ src/cc/addr.c
@@ -12,13 +12,13 @@
 #endif
 
 
-panah_sainet_s addr_new_sainet(panah_hostaddr_t host, panah_inetport_t port) {
+panah_sainet_s new_inet_addr(panah_asciizinet_t host, panah_inetport_t port) {
 	panah_inetaddr_t addr = 0;
-	addr_inet_address_to_netbyteorder(host, &addr);
+	asciizhost_to_inetaddr(host, &addr);
 
 	return (panah_sainet_s){
 		.sin_family = GLOBVAR_inet,
-		.sin_port = addr_inet_port_to_netbyteorder(port),
+		.sin_port = cshort_port_to_netorder(port),
 		.sin_addr = addr, 
 	};
 }
