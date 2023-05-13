ef35cb4347ee38c97fe4428d88c4652efd771e9e
Chubak <chubakbidpaa@gmail.com>
2023-05-13 14:23:25 +0430

    memutils added, clear and copy

---
+++ include/cc/memutils.h
@@ -0,0 +1,3 @@
+panah_nonyield_t copy_memory_from_bytewise(panah_memaddr_t dst, panah_memaddr_t src, panah_memsize_t len);
+panah_nonyield_t clear_memory_qwordwise(panah_memaddr_t addr, panah_memsize_t size);
+panah_nonyield_t clear_memory_bytewise(panah_memaddr_t addr, panah_memsize_t size);
