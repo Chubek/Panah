76b60e7da3b45bf92d6407c8edfdf84c754020f5
Chubak <chubakbidpaa@gmail.com>
2023-05-13 09:00:06 +0430

    new types for resolve, renames

--- include/cc/types.h
+++ include/cc/types.h
@@ -4,47 +4,69 @@ typedef signed char *panah_dqdata_t;
 typedef signed char *panah_asciinum_t;
 typedef signed char *panah_dnsaddr_t;
 typedef signed char *panah_dqname_t;
-typedef signed char *panah_nulltstr_t;
+typedef signed char *panah_asziiz_t;
 typedef signed char *panah_dnsresolver_t;
 typedef signed int panah_dnsttl_t;
 typedef signed int panah_yield_t;
-typedef signed char panah_asciistatic_t[21];
-typedef unsigned char panah_dqstatic_t[512];
+typedef signed char panah_asciiznum_t[21];
+typedef unsigned char panah_dqdata_t[512];
 typedef unsigned int panah_inetaddr_t;
 typedef unsigned int panah_dqtype_t;
 typedef unsigned short panah_inetport_t;
 typedef unsigned short panah_dqxid_t;
 typedef unsigned short panah_dqflag_t;
-typedef unsigned long panah_dqnumbers_t;
+typedef unsigned short panah_dqnumber_t;
+typedef unsigned long panah_inet6addr_t[2];
+typedef unsigned short panah_dqflag_t;
 typedef unsigned short panah_dqtype_t;
 typedef unsigned short panah_dqclass_t;
 typedef unsigned short panah_dqlen_t;
 typedef unsigned short panah_safamily_t;
 
-typedef struct PanahdqHeader {
+
+typedef struct PanahDNSQueryFlags {
+	panah_dqflag_t qr;
+	panah_dqflag_t opcode;
+	panah_dqflag_t aa;
+	panah_dqflag_t tc;
+	panah_dqflag_t rd;
+	panah_dqflag_t ra;
+	panah_dqflag_t z;
+	panah_dqflag_t rcode;
+} panah_dqflags_s;
+
+typedef struct PanahDNSQueryNumbers {
+	panah_dqnumber_t qdcount;
+	panah_dqnumber_t ancount;
+	panah_dqnumber_t nscount;
+	panah_dqnumber_t arcount;
+} panah_dqnumbers_s;
+
+typedef struct PanahDNSQueryHeader {
 	panah_dqxid_t queryid;
-	panah_dqflag_t queryflags;
-	panah_dqnumbers_t querynumbers;
+	panah_dqflags_s queryflags;
+	panah_dqnumbers_s querynumbers;
 } panah_dqheader_s;
 
-typedef struct PanahdqQuestion {
-	panah_dqname_t dqname;
-	panah_dqtype_t dqtype;
-	panah_dqclass_t dqclass;
+typedef struct PanahDNSQueryQuestion {
+	panah_dqname_t qname;
+	panah_dqtype_t qtype;
+	panah_dqclass_t qclass;
 } panah_dqquestion_s;
 
-typedef struct PanahdqResourceRecord {
+typedef struct PanahDNSQueryResourceRecord {
 	panah_dqname_t rname;
 	panah_dqtype_t rtype;
 	panah_dqclass_t rclass;
 	panah_dnsttl_t ttl;
+	panah_dqlen_t rdlen;
 	panah_dqdata_t rdata; 
 } panah_dqrecord_s;
 
 typedef struct PanahDNSQuery {
-	panah_dqheader_s header;
-	panah_dqquestion_s question;
-	panah_dqrecord_s record;
+	panah_dqheader_s qheader;
+	panah_dqquestion_s qquestion;
+	panah_dqrecord_s qrecord;
 } panah_dnsquery_s;
 
 typedef struct PanahINETAddress {
