2d75f0cc0eb5c9fda527ecd445931f9f28724f3e
Chubak <chubakbidpaa@gmail.com>
2023-05-13 14:24:37 +0430

    new types and macros added

--- include/cc/types.h
+++ include/cc/types.h
@@ -1,17 +1,26 @@
+#define DNSQRDATA_LEN 64
+#define DNSASCIIZ_LEN 256
+#define NUMASCIIZ_LEN 21
+#define DNSQQDATA_LEN 512
+#define INETADDR6_LEN 16
+#define INETADDR4_LEN 4
+
+
+typedef void *panah_memaddr_t;
 typedef void panah_nonyield_t;
-typedef signed char *panah_hostaddr_t;
-typedef signed char *panah_dqdata_t;
+typedef signed char panah_domainz_t[DNSASCIIZ_LEN];
+typedef signed char panah_dqrdata_t[DNSQRDATA_LEN];
+typedef signed char *panah_asciizinet_t;
 typedef signed char *panah_asciinum_t;
 typedef signed char *panah_dnsaddr_t;
-typedef signed char *panah_dqname_t;
 typedef signed char *panah_asziiz_t;
 typedef signed char *panah_dnsresolver_t;
 typedef signed int panah_dnsttl_t;
 typedef signed int panah_yield_t;
-typedef signed char panah_asciiznum_t[21];
-typedef unsigned char panah_dqdata_t[512];
+typedef signed char panah_asciiznum_t[NUMASCIIZ_LEN];
+typedef unsigned char panah_dqdata_t[DNSQQDATA_LEN];
 typedef unsigned int panah_inetaddr_t;
-typedef unsigned int panah_dqtype_t;
+typedef unsigned int panah_dqtypeclass_t;
 typedef unsigned short panah_inetport_t;
 typedef unsigned short panah_dqxid_t;
 typedef unsigned short panah_dqflag_t;
@@ -22,6 +31,7 @@ typedef unsigned short panah_dqtype_t;
 typedef unsigned short panah_dqclass_t;
 typedef unsigned short panah_dqlen_t;
 typedef unsigned short panah_safamily_t;
+typedef unsigned long panah_memsize_t;
 
 
 typedef struct PanahDNSQueryFlags {
@@ -49,24 +59,24 @@ typedef struct PanahDNSQueryHeader {
 } panah_dqheader_s;
 
 typedef struct PanahDNSQueryQuestion {
-	panah_dqname_t qname;
+	panah_domainz_t qname;
 	panah_dqtype_t qtype;
 	panah_dqclass_t qclass;
 } panah_dqquestion_s;
 
 typedef struct PanahDNSQueryResourceRecord {
-	panah_dqname_t rname;
+	panah_domainz_t rname;
 	panah_dqtype_t rtype;
 	panah_dqclass_t rclass;
 	panah_dnsttl_t ttl;
 	panah_dqlen_t rdlen;
-	panah_dqdata_t rdata; 
+	panah_dqrdata_t rdata; 
 } panah_dqrecord_s;
 
 typedef struct PanahDNSQuery {
-	panah_dqheader_s qheader;
-	panah_dqquestion_s qquestion;
-	panah_dqrecord_s qrecord;
+	panah_dqheader_s queryheader;
+	panah_dqquestion_s queryquestion;
+	panah_dqrecord_s queryrecord;
 } panah_dnsquery_s;
 
 typedef struct PanahINETAddress {
