2e06177c06312a62739169df607c433f3ed2c0db
Chubak <chubakbidpaa@gmail.com>
2023-05-13 14:23:57 +0430

    resolve done for now, header and types check

--- include/cc/resolve.h
+++ include/cc/resolve.h
@@ -1,6 +1,10 @@
-extern panah_yield_t resolve_dns_address(panah_dnsaddr_t dnsaddr, panah_dqstatic_t querydata, panah_sainet_s *resolver, panah_dqtype_t type);
-panah_yield_t parse_dqheader(panah_dqstatic_t querydata, panah_dqheader_s *dst);
-panah_yield_t parse_dqquestion(panah_dqstatic_t querydata, panah_dqquestion_s *dst);
-panah_yield_t parse_dqrecord(panah_dqstatic_t querydata, panah_dqrecord_s *dst);
-panah_yield_t parse_dnsquery(panah_dqstatic_t querydata, panah_dnsquery_s *dst);
-panah_yield_t make_dnsquery_and_get_inetaddr(panah_dnsresolver_t resolver, panah_inetport_t port, panah_inetaddr_t *dst);
+extern panah_yield_t resolve_dns_address(panah_dnsaddr_t dnsaddr, panah_dqdata_t querydata, panah_sainet_s *resolver, panah_dqtypeclass_t type);
+panah_nonyield_t parse_dqflags(panah_dqdata_t querydata, panah_dqflags_s *dst);
+panah_nonyield_t parse_dqnumbers(panah_dqdata_t querydata, panah_dqnumbers_s *dst);
+panah_nonyield_t parse_dqheader(panah_dqdata_t querydata, panah_dqheader_s *dst);
+panah_yield_t parse_dqquestion(panah_dqdata_t querydata, panah_dqquestion_s *dst);
+panah_nonyield_t parse_dqrecord(panah_dqdata_t querydata, panah_dqrecord_s *dst);
+panah_nonyield_t parse_dnsquery(panah_dqdata_t querydata, panah_dnsquery_s *dst);
+panah_yield_t make_dnsquery_and_get_rdata(panah_dnsaddr_t dnsaddr, panah_dnsresolver_t resolver, panah_inetport_t port, panah_dqtypeclass_t type, panah_dqrdata_t dst);
+panah_yield_t make_dnsquery_and_get_inetaddr(panah_dnsaddr_t dnsaddr,panah_dnsresolver_t resolver, panah_inetport_t port, panah_inetaddr_t *dst);
+panah_yield_t make_dnsquery_and_get_inet6addr(panah_dnsaddr_t dnsaddr,panah_dnsresolver_t resolver, panah_inetport_t port, panah_inet6addr_t *dst);
