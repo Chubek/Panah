3ea53d9a28e83a8d763f7e7c9e664fc30ef2d2f5
Chubak <chubakbidpaa@gmail.com>
2023-05-13 14:36:03 +0430

    more integer utilities

--- src/cc/intutils.c
+++ src/cc/intutils.c
@@ -66,3 +66,28 @@ panah_nonyield_t long_to_ascii(unsigned long number, panah_asciiznum_t asciinum)
 		number /= 10;
 	}
 }
+
+#if defined ( __TEST__ ) && defined ( __TEST_INTUTILS__ )
+
+int main() {
+	unsigned char b1 = 255;
+	unsigned char b2 = 254;
+	unsigned char b3 = 127;
+	unsigned char b4 = 1;
+	unsigned char b5 = 12;
+	unsigned char b6 = 45;
+	unsigned char b7 = 145;
+	unsigned char b8 = 12;
+
+	unsigned short wordbe = short_from_bytes_msb_left(b1, b2);
+	unsigned short wordle = short_from_bytes_msb_right(b1, b2);
+	unsigned int dwordbe = int_from_bytes_msb_left(b1, b2, b3, b4);
+	unsigned int dwordle = int_from_bytes_msb_right(b1, b2, b3, b4);
+	unsigned long qwordbe = long_from_bytes_msb_left(b1, b2, b3, b4, b5, b6, b7, b8);
+	unsigned long qwordle = long_from_bytes_msb_right(b1, b2, b3, b4, b5, b6, b7, b8);
+	unsigned long longd = ascii_to_long("12334");
+	panah_asciistatic_t asciinum;
+	long_to_ascii(longd, asciinum);
+} 
+
+#endif
