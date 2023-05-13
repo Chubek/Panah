18aed70c9033497357291601973ee1449f600ac7
Chubak <chubakbidpaa@gmail.com>
2023-05-13 08:58:51 +0430

    type name change

--- include/cc/intutils.h
+++ include/cc/intutils.h
@@ -8,4 +8,4 @@ unsigned int int_from_bytes_msb_right(unsigned char lsb, unsigned char byte1, un
 unsigned long long_from_bytes_msb_left(unsigned char lsb, unsigned char byte1, unsigned char byte2, unsigned char byte3, unsigned char byte4, unsigned char byte5, unsigned char byte6, unsigned char msb);
 unsigned long long_from_bytes_msb_right(unsigned char lsb, unsigned char byte1, unsigned char byte2, unsigned char byte3, unsigned char byte4, unsigned char byte5, unsigned char byte6, unsigned char msb);
 unsigned long ascii_to_long(panah_asciinum_t ascii);
-panah_nonyield_t long_to_ascii(unsigned long number, panah_asciistatic_t asciinum);
+panah_nonyield_t long_to_ascii(unsigned long number, panah_asciiznum_t asciinum);
