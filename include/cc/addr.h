6bcce5fe223d590438bb60b2744c0af7c9058535
Chubak <chubakbidpaa@gmail.com>
2023-05-14 08:36:17 +0430

    catch up with type rename

--- include/cc/addr.h
+++ include/cc/addr.h
@@ -1,3 +1,3 @@
-extern panah_yield_t asciizhost_to_inetaddr(panah_asciizinet_t hostaddr, panah_inetaddr_t *inetaddr);
-extern panah_inetport_t cshort_port_to_netorder(panah_inetport_t port);
-panah_sainet_s new_inet_addr(panah_asciizinet_t host, panah_inetport_t port);
+extern yield_t asciizhost_to_inetaddr(asciizinet_t hostaddr, inetaddr_t *inetaddr);
+extern inetport_t cshort_port_to_netorder(inetport_t port);
+sainet_s new_inet_addr(asciizinet_t host, inetport_t port);
